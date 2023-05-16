4268cdb8c1de4fe635153a7820f1cf4e812155b2
0xcoil <Mali-Suap-liOvy13!>
2023-05-16 23:41:19 +0200

    Chap1

--- tests/RushChapterI_test.sol
+++ tests/RushChapterI_test.sol
@@ -1,50 +1,30 @@
 // SPDX-License-Identifier: GPL-3.0
-        
+
 pragma solidity >=0.4.22 <0.9.0;
 
-// This import is automatically injected by Remix
-import "remix_tests.sol"; 
-
-// This import is required to use custom transaction context
-// Although it may fail compilation in 'Solidity Compiler' plugin
-// But it will work fine in 'Solidity Unit Testing' plugin
+import "remix_tests.sol";
 import "remix_accounts.sol";
-// <import file to test>
-import "../Rush01/RushChapterI.sol";
+import "../Rush01/RushChapter1.sol";
 
-// File name has to end with '_test.sol', this file can contain more than one testSuite contracts
-contract testSuite {
+contract RushChapterITests {
+    RushChapterI public rushChapterI;
 
-    /// 'beforeAll' runs before all other tests
-    /// More special functions are: 'beforeEach', 'beforeAll', 'afterEach' & 'afterAll'
-    function beforeAll() public {
-        // <instantiate contract>
-        Assert.equal(uint(1), uint(1), "1 should be equal to 1");
+    constructor() {
+        rushChapterI = new RushChapterI();
     }
 
-    function checkSuccess() public {
-        // Use 'Assert' methods: https://remix-ide.readthedocs.io/en/latest/assert_library.html
-        Assert.ok(2 == 2, 'should be true');
-        Assert.greaterThan(uint(2), uint(1), "2 should be greater than to 1");
-        Assert.lesserThan(uint(2), uint(3), "2 should be lesser than to 3");
-    }
+    function testFillDish() public {
+        (uint256 initialQuantity, uint256 initialRemaining) = rushChapterI.dishQuantity(RushChapterI.Dish.ICE_TEA);
 
-    function checkSuccess2() public pure returns (bool) {
-        // Use the return value (true or false) to test the contract
-        return true;
-    }
-    
-    function checkFailure() public {
-        Assert.notEqual(uint(1), uint(2), "1 should not be equal to 2");
-    }
+        // Appel de la fonction fillDish pour ajouter 5 ICE_TEA
+        rushChapterI.fillDish(RushChapterI.Dish.ICE_TEA, 5);
 
-    /// Custom Transaction Context: https://remix-ide.readthedocs.io/en/latest/unittesting.html#customization
-    /// #sender: account-1
-    /// #value: 100
-    function checkSenderAndValue() public payable {
-        // account index varies 0-9, value is in wei
-        Assert.equal(msg.sender, TestsAccounts.getAccount(1), "Invalid sender");
-        Assert.equal(msg.value, 100, "Invalid value");
+        (uint256 updatedQuantity, uint256 updatedRemaining) = rushChapterI.dishQuantity(RushChapterI.Dish.ICE_TEA);
+
+        // Verifier que la quantité initiale se maj
+        Assert.equal(updatedQuantity, initialQuantity + 5, "Incorrect updated quantity");
+
+        // Verifier que la quantité restante est bien maj 
+        Assert.equal(updatedRemaining, initialRemaining + 5, "Incorrect updated remaining quantity");
     }
 }
-    
